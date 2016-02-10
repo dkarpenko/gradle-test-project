07cf23cdb9fdf94c38f1a690a8abdccbf096d105
Denis Karpenko <denis@karpenko.me>
2016-02-10 16:32:44 +0300

    Sample gradle & junit project

---
+++ src/test/java/SomeClassTest.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class SomeClassTest {
+    @Test
+    public void foo(){
+        assertEquals("ab", "ab");
+    }
+
+    @Test
+    public void bar(){
+        assertEquals("ab", "ab");
+    }
+
+    @Test
+    public void bar1(){
+        assertEquals("ab", "ab");
+    }
+
+}
